b304a646f9323df320920d9553da4269a561d86b
barry <barry.su@avantport.com>
2015-06-18 17:49:40 +0800

    add TT.java

---
+++ src/com/test/TT.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.Random;
+
+public class TT {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int number = new Random().nextInt(10);
+        System.out.println("number:"+number);
+    }
+
+}
